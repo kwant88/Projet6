c3c4e77787c8fbb16cbea33c0b74c17d368ac1c6
kwant88 <85560278+kwant88@users.noreply.github.com>
2022-04-07 13:49:41 +0200

    Config mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+
+mongoose.connect('mongodb+srv://kwant8888:<password>@cluster0.qu9uo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Middleware permettant au front et back end de communiquer entre eux
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
